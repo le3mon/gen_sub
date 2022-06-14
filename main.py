913619cd68ce0743f92f67541faf382efa05d368
lee3mon <lee3mon@gmail.com>
2022-06-14 19:01:10 +0900

    Edit code

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from subtitle import *
-import json
 
 sub = Subtitle()
 sub.video_capture('sample3.mp4')
-sub.set_option('mp4v', 60)
+sub.set_video_option('mp4v', 60)
 sub.video_writer('output.mp4')
 sub.set_subtitles('sample.json')
 sub.edit()
